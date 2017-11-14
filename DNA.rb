69c8b90fe16aa1a7798acc3c8c864a3e47118474
Izabela <BadbCatha@users.noreply.github.com>
2017-11-14 23:17:14 +0100

    Added

---
+++ DNA.rb
@@ -0,0 +1,16 @@
+﻿=begin
+Zasada komplementarności DNA mówi, że znając sekwencję pojedyńczego łańcucha DNA możemy wywnioskować sekwencję jego sąsiedniej nici.
+Wynika to z faktu, iż zasady azotowe, z których DNA jest zbudowane łączą się w paryzawsze w ten sam sposób,
+to znaczy Adenina (A) z Tyminą (T), a Cytozyna C z Guaniną(G).
+Napisz funckję, która przyjmie ciąg znaków reprezentujący sekwencję DNA i zwrócisekwencję komplementarną.
+
+=end
+
+def complementary_DNA(kod)
+ komplementarny = kod.gsub(/[ATCG]/, 'A' => 'T', 'T' => 'A', 'C' => 'G', 'G' => 'C') 
+ puts komplementarny
+end
+ 
+
+
+complementary_DNA("ATTA")#=> "TAAT"complementary_DNA("CGGC")#=> "GCCG"
